ceeb3afa6c20ced100bf92dafd566cb958e15643
Venusai <89316212+venu1201@users.noreply.github.com>
2024-01-01 16:40:53 +0530

    Update Topnavbar.jsx

--- client/src/components/navigationbar/Topnavbar.jsx
+++ client/src/components/navigationbar/Topnavbar.jsx
@@ -9,7 +9,7 @@ import { BiLogOut } from "react-icons/bi";
 
 import { CgProfile, CgAddR } from "react-icons/cg";
 import { clear_google } from '../../reducers';
-const Navbar = () => {
+const Topnavnar = () => {
   const dispatch = useDispatch();
   const authData = useSelector((state) => state.authData);
   if (authData === null) {
@@ -90,4 +90,4 @@ const Navbar = () => {
   )
 }
 
-export default Navbar
+export default Topnavbar
